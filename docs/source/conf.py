38821ad047a3956872d607fb2d75c297be61f227
Xander Wilcke <w.x.wilcke@vu.nl>
2023-05-04 14:37:39 +0200

    updated documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,4 +30,5 @@ exclude_patterns = []
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
+master_doc = 'index'  # needed by rtd
 html_static_path = ['_static']
